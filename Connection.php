72531c67dc9250ee9dacd9c378c5f8a5f91a8931
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2023-05-16 10:48:56 -0500

    Se inicio el arreglo del error, NO TOCAR!!!

--- Connection.php
+++ Connection.php
@@ -1,14 +1,3 @@
-
-<!--                                                                                            _____
-                                                                                              /       \
-                                                                                             /  STOP   \
-                                                                                             \         /
-                                                                                              \ _____ /
-                                                                                                 |
-                                                                                                 |
-                                                                                        | NO PASAR SIN PERMISO |
-________________________________________________________________________________________________________________________________________________________________________________________________/ -->
-                                                           <!-- No Tocar nada a menos que estes 100% seguro de lo que haces... -->
 <?php
 
 class conection
@@ -16,27 +5,46 @@ class conection
     public $server = 'sql10.freemysqlhosting.net';
     public $user = "sql10618119";
     public $password = "4NHAsCAvnP";
-    public $dbname = 'sql10618119';
 
     public $conection;
 
 
     public function __construct()
     {
+        try{
+            $username="sql10618119";
+            $password="4NHAsCAvnP";
+            $database="sql10618119";
 
-        try {
-            $this->conection= new PDO("mysql:host=" . $this->server . ";dbname=	sql10618119", $this->user, $this->password);
-            $this->conection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            echo 'error' . $e."UNA LOCURA...";
+            $mysqli = new mysqli("sql10.freemysqlhosting.net", $username, $password, $database);
+
+            //echo $mysqli->query("SELECT * FROM `imagenes`") ."funciono" ;
+            foreach($mysqli->query("SELECT * FROM `imagenes`") as $clave => $valor) {
+                print "$clave => $valor\n";
+                foreach ($valor as &$valor1) {
+                    print($valor1);
+                }
+            }
+            echo "\n";
+
+        }catch (PDOException $e) {
+            echo 'error' . $e;
         }
+
+
+        //try {
+        //    $this->conection= new PDO("mysql:host=" . $this->server . ";dbname=	sql10618119", $this->user, $this->password);
+        //    $this->conection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //} catch (PDOException $e) {
+        //    echo 'error' . $e;
+        //}
     }
     public function execut($I)//Insertar/actualizar/Eliminar
     {
         $this->conection->exec($I); 
         return $this->conection->lastInsertId();
     }
-    public function consult($sql="SELECT * FROM `imagenes`")
+    public function consult($sql)
     {
         $sentence = $this->conection->prepare($sql);
         $sentence->execute();
--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
-include("Heads.php")
+include("Headboard.php")
 ?>
 <?php
 include("Connection.php")
 ?>
 <?php
 $objconection = new conection;
-$result = $objconection->consult("SELECT * FROM `imagenes`");
+$result = $objconection->consult("SELECT * FROM `proyectos`");
 ?>
 
 <div class="p-5 mb-4 bg-light rounded-3">
   <div class="container-fluid py-5">
     <h1 class="display-5 fw-bold">Bienvenid@s</h1>
-    <p class="col-md-8 fs-4">Este es una API en PHPcon imagenes de campuslands</p>
-    <!-- <button class="btn btn-primary btn-lg" type="button">Más información</button> -->
+    <p class="col-md-8 fs-4">Este es un portafolio privado</p>
+    <button class="btn btn-primary btn-lg" type="button">Más información</button>
   </div>
 </div>
 
@@ -27,9 +27,9 @@ $result = $objconection->consult("SELECT * FROM `imagenes`");
 
   <div class="col">
     <div class="card h-100">
-      <img src="Archivos/<?php echo $project['Link']; ?>" class="card-img-top" alt="...">
+      <img src="Archivos/<?php echo $project['image']; ?>" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title"><?php echo $project['Name']; ?></h5>
+        <h5 class="card-title"><?php echo $project['name']; ?></h5>
         <p class="card-text"><?php echo $project['Description']; ?></p>
       </div>
     </div>
